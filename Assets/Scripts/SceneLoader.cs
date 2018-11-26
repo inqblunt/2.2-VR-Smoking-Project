6b174a89c0087d1732417718342aa2a053f477e3
inqblunt <39767525+inqblunt@users.noreply.github.com>
2018-11-26 01:10:50 +0100

    new repo

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void LoadByIndex(int sceneIndex) {
+        SceneManager.LoadScene(sceneIndex);
+	}
+
+}
